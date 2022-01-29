f57ef641d6c4086610548390320ff8800c6fe357
mateu <mateusz.wygoda>
2022-01-29 22:47:55 +0100

    Full logci in Measure switcher

--- src/MeasureSwitcher.java
+++ src/MeasureSwitcher.java
@@ -1,2 +1,30 @@
 public class MeasureSwitcher {
+
+    double meterToCentimeter(double a){
+        return a*100;
+    }
+
+    double meterToMillimeter(double a){
+        return a*1000;
+    }
+
+    double centimeterToMeter(double a){
+        return a/100;
+    }
+
+    double millimeterToMeter(double a){
+        return a/1000;
+    }
+
+    int hourToMinute(int a){
+        return a*60;
+    }
+
+    int minuteToSec(int a){
+        return a*60;
+    }
+
+    int secToMillisec(int a){
+        return a*1000;
+    }
 }
