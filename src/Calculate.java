8f5cec758fc00b63cc7c90d894563a0fcb998885
mateu <mateusz.wygoda>
2022-01-29 23:02:04 +0100

    Add test for meter logic

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,5 +1,16 @@
 public class Calculate {
     public static void main(String[] args) {
+        MeasureSwitcher mswitcher = new MeasureSwitcher();
+        double a = 4.3;
+
+        System.out.println("A = "+a);
+
+        System.out.println(a+" meter to centimeter: "+mswitcher.meterToCentimeter(a));
+        System.out.println((a+" meter to millimeter: "+mswitcher.meterToMillimeter(a)));
+
+        System.out.println(mswitcher.meterToCentimeter(a)+" centimeter to meter "+mswitcher.centimeterToMeter(mswitcher.meterToCentimeter(a)));
+        System.out.println(mswitcher.meterToMillimeter(a) +" millimeter to meter: "+mswitcher.millimeterToMeter(mswitcher.meterToMillimeter(a)));
+
 
     }
 }
